4b17d2d5b8e62d2d762a59fc3facda5aade0ff61
Lesley-Byte <phosphorescence3@hotmail.com>
2022-09-12 22:49:35 -0400

    Create index.py

---
+++ examples/index.py
@@ -0,0 +1,18 @@
+html = """<!DOCTYPE html>
+<html lang="en">
+    <head> 
+        <meta charset="UTF-8"/>
+        <link rel="stylesheet" href="/stylefile.css">
+        <title>Pico W</title>
+    </head>
+    <body>
+        <main>
+        <h1>Pico</h1>
+        <p>%s</p>
+        <a href="/light/on">Light On</a> </br>
+        <a href="/light/off">Light Off</a> </br>
+        </main>
+        <script type="text/javascript" src="script.js"></script>
+    </body>
+</html>
+"""
